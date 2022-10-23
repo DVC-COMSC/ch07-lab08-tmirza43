b47acd2471961a05a9e83244512eb384b61b98fc
Talha Mirza <tmirza43@gmail.com>
2022-10-23 15:51:03 -0700

    commit1

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # ******************************
 # Make your Code
 # ******************************
+
+numbers = input("Enter 5 int in ascending order: ").split()
+numbers = list(map(int, numbers))
+new = int(input("Enter another value: "))
+
+for i in range(5):
+  if numbers[i] > new:
+    numbers.insert(i, new)
+    break
+
+print(numbers)
